77b3a3494a7f42ed39da0c61bb8f9885de9827e5
wuzhibin <156669011@qq.com>
2019-04-06 23:56:22 +0800

    20190406头尾完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import MintUI from 'mint-ui';
+import 'mint-ui/lib/style.css';
+import router from './router';
+import './lib/mui/css/mui.min.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(MintUI);
 
 new Vue({
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
